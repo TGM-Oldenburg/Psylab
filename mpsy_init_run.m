f235c31a9b860417d2ac5b216a81a3ffe0680dcd
Martin Hansen <martin.hansen@jade-hs.de>
2016-04-27 17:44:43 +0200

    automatic generation of interstimulus signals

--- mpsy_init_run.m
+++ mpsy_init_run.m
@@ -46,6 +46,22 @@ M.UPPER_REV_IDX   = [];
 M.DIRECTION = [];
 %
 
+% generate silence signals, in case only their duration is specified.
+% NOTE: this works only in case of mono signals
+if isscalar(m_quiet),
+  m_quiet = zeros(round(m_quiet*M.FS), 1);
+  disp('hu')
+end
+if isscalar(m_postsig),
+  m_postsig = zeros(round(m_postsig*M.FS), 1);
+  disp('ha')
+end
+if isscalar(m_presig),
+  m_presig = zeros(round(m_presig*M.FS), 1);
+  disp('he')
+end
+
+
 % reset some internal variables 
 % NOTE:  mpsy_proto and mpsy_plot_feedback depend on THIS:
 M.med_thres = [];
